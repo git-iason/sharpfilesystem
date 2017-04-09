962fd628a90ed5fd4c69a5900d4c5eb215cf3e78
iason leiloglou <leiloglou@gmail.com>
2017-04-09 13:59:42 -0400

    added a seeking stream so that i can support file systems off of embedde zips. not sure if this is the way to go

--- SharpFileSystem.SharpZipLib/SharpZipLibFileSystem.cs
+++ SharpFileSystem.SharpZipLib/SharpZipLibFileSystem.cs
@@ -13,12 +13,15 @@ namespace SharpFileSystem.SharpZipLib
 
         public static SharpZipLibFileSystem Open(Stream s)
         {
-            return new SharpZipLibFileSystem(new ZipFile(s));
+            var zip = new ZipFile(s) {UseZip64 = UseZip64.Off};
+            return new SharpZipLibFileSystem(zip);
         }
 
         public static SharpZipLibFileSystem Create(Stream s)
         {
-            return new SharpZipLibFileSystem(ZipFile.Create(s));
+            var zip = ZipFile.Create(s);
+            zip.UseZip64 = UseZip64.Off;
+            return new SharpZipLibFileSystem(zip);
         }
 
         private SharpZipLibFileSystem(ZipFile zipFile)
@@ -78,16 +81,37 @@ namespace SharpFileSystem.SharpZipLib
 
         public Stream CreateFile(FileSystemPath path)
         {
+            //BeginUpdate is required before adding entries to the zip file
+            ZipFile.BeginUpdate();
+
             var entry = new MemoryZipEntry();
             ZipFile.Add(entry, ToEntryPath(path));
-            return entry.GetSource();
+             return PrepareStream(entry.GetSource());
+           // return entry.GetSource();
         }
 
+        private SeekStream PrepareStream(Stream entry)
+        {
+            var stream = new SeekStream(entry);
+            stream.Seek(0, SeekOrigin.End);
+            //seek back to the beginning so the stream is ready to be read from or written to
+            stream.Seek(0, SeekOrigin.Begin);
+
+            stream.DataWritten += FinishUpdate;
+            return stream;
+        }
+
+        public void FinishUpdate()
+        {
+            if (ZipFile.IsUpdating)
+                ZipFile.CommitUpdate();
+        }
         public Stream OpenFile(FileSystemPath path, FileAccess access)
         {
-            if (access != FileAccess.Read)
-                throw new NotSupportedException();
-            return ZipFile.GetInputStream(ToEntry(path));
+            if (access == FileAccess.Write || access==FileAccess.ReadWrite)
+                ZipFile.BeginUpdate();
+           
+            return PrepareStream(ZipFile.GetInputStream(ToEntry(path)));
         }
 
         public void CreateDirectory(FileSystemPath path)
--- SharpFileSystem.Tests/SharpZipLib/SharpZipLibFileSystemTest.cs
+++ SharpFileSystem.Tests/SharpZipLib/SharpZipLibFileSystemTest.cs
@@ -22,7 +22,7 @@ namespace SharpFileSystem.Tests.SharpZipLib
             var memoryStream = new MemoryStream();
             zipStream = memoryStream;
             var zipOutput = new ZipOutputStream(zipStream);
-
+            zipOutput.UseZip64 = UseZip64.Off;
             var fileContentString = "this is a file";
             var fileContentBytes = Encoding.ASCII.GetBytes(fileContentString);
             zipOutput.PutNextEntry(new ZipEntry("textfileA.txt")
@@ -32,7 +32,6 @@ namespace SharpFileSystem.Tests.SharpZipLib
             zipOutput.Write(fileContentBytes);
             zipOutput.PutNextEntry(new ZipEntry("directory/fileInDirectory.txt"));
             zipOutput.Finish();
-
             memoryStream.Position = 0;
             fileSystem = SharpZipLibFileSystem.Open(zipStream);
         }
@@ -77,6 +76,86 @@ namespace SharpFileSystem.Tests.SharpZipLib
             Assert.IsFalse(fileSystem.Exists(FileSystemPath.Parse("/nonExistingFile")));
             Assert.IsFalse(fileSystem.Exists(FileSystemPath.Parse("/nonExistingDirectory/")));
             Assert.IsFalse(fileSystem.Exists(FileSystemPath.Parse("/directory/nonExistingFileInDirectory")));
+        }
+
+        [Test]
+        public void EmbeddedZipTest()
+        {
+            var memoryStream = new MemoryStream();
+            zipStream = memoryStream;
+            var zipOutput = new ZipOutputStream(zipStream);
+
+            var fileContentString = "this is a file";
+            var fileContentBytes = Encoding.ASCII.GetBytes(fileContentString);
+            zipOutput.PutNextEntry(new ZipEntry("textfileA.txt")
+            {
+                Size = fileContentBytes.Length
+            });
+            zipOutput.Write(fileContentBytes);
+            zipOutput.PutNextEntry(new ZipEntry("directory/fileInDirectory.txt"));
+            zipOutput.Finish();
+            memoryStream.Position = 0;
+
+            var zipData = zipStream.ReadAllBytes();
+            var fs = fileSystem.CreateFile(FileSystemPath.Parse("/internalZip.zip"));
+            fs.Write(zipData);
+            fs.Close();
+
+            Assert.IsTrue(fileSystem.Exists(FileSystemPath.Parse("/internalZip.zip")));
+
+
+            var zip = fileSystem.OpenFile(FileSystemPath.Parse("/internalZip.zip"), FileAccess.Read);
+
+           Assert.IsTrue(zip.Length>0);
+            var newFs = SharpZipLibFileSystem.Open(zip);
+
+            var textFile = newFs.OpenFile(textfileAPath, FileAccess.Read);
+            var text = textFile.ReadAllText();
+            Assert.IsTrue(string.Equals(text,fileContentString));
+        }
+
+        [Test]
+        public void MultiWriteMultiReadTest()
+        {
+            var fileA = FileSystemPath.Parse("/file.txt");
+            var fileB = FileSystemPath.Parse("/fileB.txt");
+            var fileC = FileSystemPath.Parse("/fileC.txt");
+
+            var fileContentStringA = "this is a file";
+            var fileContentStringB = "this is b file";
+            var fileContentStringC = "this is c file";
+            var fileContentBytes = Encoding.ASCII.GetBytes(fileContentStringA);
+            var fileContentBytesB = Encoding.ASCII.GetBytes(fileContentStringB);
+            var fileContentBytesC = Encoding.ASCII.GetBytes(fileContentStringC);
+
+            var fs = fileSystem.CreateFile(fileA);
+            fs.Write(fileContentBytes);
+            fs.Close();
+
+
+            fs = fileSystem.CreateFile(fileB);
+            fs.Write(fileContentBytesB);
+            fs.Close();
+
+            Assert.IsTrue(fileSystem.Exists(fileB));
+
+            var textFile = fileSystem.OpenFile(fileB, FileAccess.Read);
+            var text = textFile.ReadAllText();
+            Assert.IsTrue(string.Equals(text, fileContentStringB));
+
+
+            Assert.IsTrue(fileSystem.Exists(fileA));
+
+            textFile = fileSystem.OpenFile(fileA, FileAccess.ReadWrite);
+            text = textFile.ReadAllText();
+            Assert.IsTrue(string.Equals(text, fileContentStringA));
+
+            textFile.Write(fileContentBytesC);
+            textFile.Close();
+
+            textFile = fileSystem.OpenFile(fileA, FileAccess.ReadWrite);
+            text = textFile.ReadAllText();
+            Assert.IsTrue(string.Equals(text, fileContentStringC));
         }
     }
 }
